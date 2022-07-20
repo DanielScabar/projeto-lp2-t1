cbbe3f10ae3614d5d494bf3c4b47d7d8417de2bc
Daniel Scabar <danielscabar@pop-os.localdomain>
2022-07-20 12:06:45 -0300

    Componente cabecalho estilizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LivrosCadastroComponent } from './livros/livros-cadastro/livros-cadastro.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LivrosCadastroComponent } from './livros/livros-cadastro/livros-cadastro.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,8 @@ import { CabecalhoComponent } from './cabecalho/cabecalho.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
