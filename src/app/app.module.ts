c4135d9fd1963fea2d0238c28c962421751b1048
Daniel Scabar <danielscabar@pop-os.localdomain>
2022-07-20 11:06:00 -0300

    Component Cadastro de Livros criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LivrosCadastroComponent } from './livros/livros-cadastro/livros-cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LivrosCadastroComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/livros/livros-cadastro/livros-cadastro.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'app-livros-cadastro',
+  templateUrl:'./livros-cadastro.component.html',
+})
+
+export class LivrosCadastroComponent {}
