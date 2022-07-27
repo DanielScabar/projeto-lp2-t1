55195f595ce5a5b11a29e31bee87512105c68720
Daniel Scabar <danielscabar@pop-os.localdomain>
2022-07-27 11:17:47 -0300

    Método para remover registro do Observable

--- src/app/livros/livros-lista/livros-lista.component.ts
+++ src/app/livros/livros-lista/livros-lista.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Livro } from '../livro.model';
 import { LivroService } from '../livro.service';
+import { Subscription,Observable } from 'rxjs';
 
 @Component({
   selector: 'app-livros-lista',
   templateUrl: './livros-lista.component.html',
   styleUrls: ['./livros-lista.component.css'],
 })
-export class LivrosListaComponent implements OnInit {
+export class LivrosListaComponent implements OnInit, OnDestroy {
   livros: Livro[] = [];
+  private livrosSubscription: Subscription;
 
   constructor(public livroService: LivroService) {}
 
   ngOnInit(): void {
     this.livros = this.livroService.getLivros();
-    this.livroService
+    this.livrosSubscription = this.livroService
       .getListaDeLivrosAtualizadaObservable()
       .subscribe((livros: Livro[]) => {
         this.livros = livros;
       });
+  }
+
+  ngOnDestroy(): void {
+    this.livrosSubscription.unsubscribe();
   }
 }
