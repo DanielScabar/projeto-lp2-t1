ab781dba6c0ae73f0e616134c01bca9e7359d4ef
Daniel Scabar <danielscabar@pop-os.localdomain>
2022-07-29 13:39:08 -0300

    Substituindo template driven forms por reactive forms

--- src/app/livros/livros-cadastro/livros-cadastro.component.ts
+++ src/app/livros/livros-cadastro/livros-cadastro.component.ts
@@ -1,68 +1,84 @@
-import {Component, OnInit} from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Livro } from '../livro.model';
-import {LivroService} from '../livro.service';
+import { LivroService } from '../livro.service';
 
 @Component({
-  selector:'app-livros-cadastro',
-  templateUrl:'./livros-cadastro.component.html',
-  styleUrls:['./livros-cadastro.component.css']
+  selector: 'app-livros-cadastro',
+  templateUrl: './livros-cadastro.component.html',
+  styleUrls: ['./livros-cadastro.component.css'],
 })
-
-export class LivrosCadastroComponent implements OnInit{
-
-  private modo:string = "criar";
+export class LivrosCadastroComponent implements OnInit {
+  private modo: string = 'criar';
   private idLivro: string;
   public livro: Livro;
   public estaCarregando: boolean = false;
+  form: FormGroup;
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      titulo: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)],
+      }),
+      autor: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      npaginas: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      if(paramMap.has("idLivro")){
-        this.modo = "editar";
-        this.idLivro = paramMap.get("idLivro");
+      if (paramMap.has('idLivro')) {
+        this.modo = 'editar';
+        this.idLivro = paramMap.get('idLivro');
         this.estaCarregando = true;
-        this.livroService.getLivro(this.idLivro).subscribe(dadosLiv => {
+        this.livroService.getLivro(this.idLivro).subscribe((dadosLiv) => {
           this.estaCarregando = false;
           this.livro = {
             id: dadosLiv._id,
             titulo: dadosLiv.titulo,
             autor: dadosLiv.autor,
-            npaginas: dadosLiv.npaginas
-          }
+            npaginas: dadosLiv.npaginas,
+          };
+          this.form.setValue({
+            titulo: this.livro.titulo,
+            autor: this.livro.autor,
+            npaginas: this.livro.npaginas,
+          });
         });
-      }
-      else{
-        this.modo = "criar";
+      } else {
+        this.modo = 'criar';
         this.idLivro = null;
       }
-    })
+    });
   }
 
-  constructor(public livroService:LivroService, public route: ActivatedRoute){}
+  constructor(
+    public livroService: LivroService,
+    public route: ActivatedRoute
+  ) {}
 
-  onCadastrarLivro(form: NgForm){
-    if(form.invalid){
+  onCadastrarLivro() {
+    if (this.form.invalid) {
       return;
     }
     this.estaCarregando = true;
-    if(this.modo === "criar"){
+    if (this.modo === 'criar') {
       this.livroService.setLivro(
-        form.value.titulo,
-        form.value.autor,
-        form.value.npaginas
+        this.form.value.titulo,
+        this.form.value.autor,
+        this.form.value.npaginas
       );
-    }
-    else{
+    } else {
       this.livroService.atualizarLivro(
         this.idLivro,
-        form.value.titulo,
-        form.value.autor,
-        form.value.npaginas
-      )
+        this.form.value.titulo,
+        this.form.value.autor,
+        this.form.value.npaginas
+      );
     }
-    
-    form.resetForm();
+
+    this.form.reset();
   }
 }
